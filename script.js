f19bbfb2e7552863576b5e121678365a4aaef36f
Cody Riebel <cody.riebel@gmail.com>
2023-08-24 21:25:24 -0400

    feat: hover event

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-let gridContainer = document.querySelector('#gridContainer');
+let hover = (event) => event.target.classList.toggle('hover');
+
+let gridContainer = document.querySelector('#grid');
 
 for (row = 0; row < 16; row++) {
   for (col = 0; col < 16; col++) {
     let square = document.createElement('div');
     square.classList.add('square')
     gridContainer.appendChild(square)
+    square.addEventListener('mouseenter', hover)
+    square.addEventListener('mouseleave', hover)
   }
 }
+

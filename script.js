35c5317ce34771c72fc4fc6cb089b03b58bbd62c
Cody Riebel <cody.riebel@gmail.com>
2023-08-26 19:46:20 -0400

    basic design

--- script.js
+++ script.js
@@ -4,16 +4,42 @@ let colorSquare = (event) => {
   event.target.style.backgroundColor = 'black';
 }
 
-let gridContainer = document.querySelector('#grid');
+let removeGrid = (size) => {
+  for (i = 0; i < size*size; i++) {
+    gridContainer.removeChild(square);
+  }
+}
 
-for (row = 0; row < 16; row++) {
-  for (col = 0; col < 16; col++) {
-    let square = document.createElement('div');
+let createGrid = (size) => {
+  for (i = 0; i < size*size; i++) {
+    square = document.createElement('div');
     square.classList.add('square')
     gridContainer.appendChild(square)
     square.addEventListener('mouseenter', hover)
     //square.addEventListener('mouseleave', hover)
-    //square.addEventListener('click', colorSquare)
+    //square.addEventListener('click', colorSquare) 
   }
 }
 
+let gridEvent = () => {
+  removeGrid(numSquare);
+  while (true) {
+    numSquare = prompt('How many squares per side? (1 - 100)', '16');
+    if (parseInt(numSquare) > 100) {
+      alert('MAX 100');
+    } else if (parseInt(numSquare) < 1) {
+      alert('MIN 0')
+    } else break;
+  }
+  createGrid(numSquare);
+}
+
+let gridContainer = document.querySelector('#grid');
+
+let square;
+let numSquare = 16;
+
+let squareChoice = document.querySelector('#buttonNumSquare');
+squareChoice.addEventListener('click', gridEvent)
+
+createGrid(numSquare);

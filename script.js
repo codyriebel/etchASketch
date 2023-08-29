72a59cbaa4f65daf646b09b233e33db43fe2f139
Cody Riebel <cody.riebel@gmail.com>
2023-08-28 22:53:38 -0400

    feat: randomcolor, color picker, # squares, clear; todo: fix shading

--- script.js
+++ script.js
@@ -1,22 +1,71 @@
-let color = (event) => event.target.classList.add('color');
+let color = (event) => {
+  if (touches > 0) {
+    touches -= 1;
+  };
 
+  if (randomColor === true) {
+    clr = getRandomColor();
+    event.target.style.backgroundColor = (clr);
+  } else if (choseAColor === true) {
+    let chosen = document.querySelector('#chooseColor');
+    event.target.style.backgroundColor = chosen.value;
+  } else if (grayscale === true ) {
+    event.target.style.backgroundColor = `#${touches}${touches}${touches}`;
+  }
+}
+
+function getRandomColor() {
+  let letters = '0123456789ABCDEF';
+  let hexcolor = '#';
+  for (let i = 0; i < 6; i++) {
+    hexcolor += letters[Math.floor(Math.random() * 16)];
+  }
+  return hexcolor;
+}
+
+let randomColors = () => {
+  choseAColor = false;
+  if (randomColor === false) {
+    randomColor = true;
+    grayscale = false;
+    buttonRandomColors.textContent = 'grayscale';
+  } else {
+    randomColor = false;
+    grayscale = true;
+    buttonRandomColors.textContent = 'random colors';
+  }
+}
+
+let chooseColor = () => {
+  choseAColor = true;
+  randomColor = false;
+  grayscale = false;
+  choice.type = 'color';
+  colorChoice.remove();
+  let buttonContainer = document.querySelector('.buttonContainer');
+  buttonContainer.appendChild(choice);
+}
 
 let removeGrid = () => {
-  let squares = document.querySelectorAll('.square')
-  squaresArr = Array.from(squares);
-  for (square of squaresArr) {
-    document.querySelector('#grid').removeChild(square);
+  let rows = document.querySelectorAll('.rowContainer');
+  let rowsArr = Array.from(rows);
+  for (r of rowsArr) {
+    r.remove();
   }
 }
 
 let createGrid = (size) => {
   let grid = document.querySelector('#grid');
-  for (i = 0; i < size*size; i++) {
-    let square = document.createElement('div');
-    square.classList.add('square')
-    grid.appendChild(square)
-    square.addEventListener('mouseenter', color)
-    //square.addEventListener('mouseleave', hover) 
+  for (col = 0; col < size; col++) {
+    let rowContainer = document.createElement('div');
+    rowContainer.classList.add('rowContainer');
+    grid.appendChild(rowContainer);
+    for (row = 0; row < size; row++) {
+      let square = document.createElement('div');
+      square.classList.add('square');
+      rowContainer.appendChild(square);
+      square.addEventListener('mouseenter', color);
+    }
   }
 }
 
@@ -34,13 +83,38 @@ let getGridSize = () => {
     if (parseInt(numSquare) > 100) {
       alert('MAX 100');
     } else if (parseInt(numSquare) < 1) {
-      alert('MIN 0')
+      alert('MIN 0');
     } 
     return numSquare;
+  }
+}
+
+let clearGrid = () => {
+  let squares = document.querySelectorAll('.square');
+  let squaresArr = Array.from(squares);
+  for (s of squaresArr) {
+    s.style.backgroundColor = '#fff';
   }
 }
 
 createGrid(16);
 
 let buttonNumSquare = document.querySelector('#buttonNumSquare');
-buttonNumSquare.addEventListener('click', newGrid)
+buttonNumSquare.addEventListener('click', newGrid);
+
+let buttonRandomColors = document.querySelector('#buttonRandomColors');
+buttonRandomColors.addEventListener('click', randomColors);
+
+let randomColor = false;
+
+let grayscale = true;
+
+let colorChoice = document.querySelector('#chooseColor');
+colorChoice.addEventListener('click', chooseColor);
+
+let choseAColor = false;
+
+let clear = document.querySelector('#clear');
+clear.addEventListener('click', clearGrid)
+
+let touches = 10;
